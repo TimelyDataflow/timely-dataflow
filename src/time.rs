a0f08b0fcadbeff4ac60f7663ea588ad2043ffd1
Frank McSherry <fmcsherry@me.com>
2014-12-07 11:56:43 -0800

    cleaning up

--- src/time.rs
+++
@@ -1,9 +0,0 @@
-use core::fmt::Show;
-use std::hash::Hash;
-use std::default::Default;
-
-
-pub trait Time: Ord+Eq+PartialOrd+PartialEq+Copy+Default+Hash+Show+'static { }
-
-impl Time for () { }
-impl Time for uint { }
