0841af87faef8a5d6d137a0e4322462325c4e38f
Frank McSherry <fmcsherry@me.com>
2015-04-04 11:07:44 +0200

    fixed trait name

---
+++ src/example/select.rs
@@ -0,0 +1,27 @@
+use progress::Graph;
+use communication::Pullable;
+use communication::channels::Data;
+use communication::exchange::{ParallelizationContract, Pipeline};
+use communication::observer::ObserverSessionExt;
+use example::stream::Stream;
+use example::unary::UnaryExt;
+
+// a trait enabling the "select" method
+pub trait SelectExt<G: Graph, D1: Data, D2: Data> {
+    fn select<L: Fn(D1) -> D2+'static>(&mut self, logic: L) -> Stream<G, D2>;
+}
+
+// implement the extension trait for Streams
+impl<G: Graph, D1: Data, D2: Data> SelectExt<G, D1, D2> for Stream<G, D1> {
+    fn select<L: Fn(D1) -> D2+'static>(&mut self, logic: L) -> Stream<G, D2> {
+        let (sender, receiver) = Pipeline.connect(&mut self.graph.communicator());
+        self.unary(sender, receiver, format!("Select"), move |handle| {
+            while let Some((time, data)) = handle.input.pull() {
+                let mut session = handle.output.session(&time);
+                for datum in data {
+                    session.push(&logic(datum));
+                }
+            }
+        })
+    }
+}
