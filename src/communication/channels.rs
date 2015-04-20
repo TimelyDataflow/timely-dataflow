0966ac937556d0abe898ef4a0d1212d31a7dff88
Frank McSherry <fmcsherry@me.com>
2015-04-20 14:58:12 +0200

    fixed memory leak in OutputPort

--- src/communication/channels.rs
+++ src/communication/channels.rs
@@ -6,46 +6,12 @@ use progress::Timestamp;
 use progress::count_map::CountMap;
 
 use communication::Observer;
-// use communication::observer::{BufferedObserver, FlattenedObserver, BroadcastObserver};
 
 use std::rc::Rc;
 use std::cell::RefCell;
 
 pub trait Data : Clone+Send+Debug+Any { }
 impl<T: Clone+Send+Debug+Any> Data for T { }
-
-// // TODO : All the buffering, cloning, and dropping makes pandas sad.
-// // TODO : It should be easy enough to Rc<RefCell<Vec<Vec<D>>>> things up.
-// // TODO : OutputPort can do the buffering, broadcasting, and collecting vecs.
-// pub struct OutputPort<T: Timestamp, D: Data> {
-//     shared: BufferedObserver<D, Rc<RefCell<BroadcastObserver<Box<Observer<Time=T, Data=Vec<D>>>>>>>,
-// }
-//
-// impl<T: Timestamp, D: Data> Observer for OutputPort<T, D> {
-//     type Time = T;
-//     type Data = D;
-//     #[inline(always)] fn open(&mut self, time: &T) { self.shared.open(time); }
-//     #[inline(always)] fn show(&mut self, data: &D) { self.shared.show(data); }
-//     #[inline(always)] fn give(&mut self, data:  D) { self.shared.give(data); }
-//     #[inline(always)] fn shut(&mut self, time: &T) { self.shared.shut(time); }
-// }
-//
-// impl<T: Timestamp, D: Data> OutputPort<T, D> {
-//     pub fn new() -> OutputPort<T, D> {
-//         OutputPort { shared: BufferedObserver::new(256, Rc::new(RefCell::new(BroadcastObserver::new()))) }
-//     }
-//     pub fn add_observer<O: Observer<Time=T, Data=D>+'static>(&self, observer: O) {
-//         self.shared.inner().borrow_mut().add(Box::new(FlattenedObserver::new(observer)));
-//     }
-// }
-//
-// impl<T: Timestamp, D: Data> Clone for OutputPort<T, D> {
-//     fn clone(&self) -> OutputPort<T, D> {
-//         OutputPort {
-//             shared: BufferedObserver::new(256, self.shared.inner().clone()),
-//         }
-//     }
-// }
 
 pub struct OutputPort<T: Timestamp, D: Data> {
     limit:  usize,
@@ -58,41 +24,23 @@ impl<T: Timestamp, D: Data> Observer for OutputPort<T, D> {
     type Time = T;
     type Data = D;
     #[inline(always)] fn open(&mut self, time: &T) {
-        for observer in self.shared.borrow_mut().iter_mut() {
-            observer.open(time);
-        }
+        for observer in self.shared.borrow_mut().iter_mut() { observer.open(time); }
     }
     #[inline(always)] fn show(&mut self, data: &D) { self.give(data.clone()); }
     #[inline(always)] fn give(&mut self, data:  D) {
         self.buffer.push(data);
-        if self.buffer.len() > self.limit {
-            let mut observers = self.shared.borrow_mut();
-            for index in (0..observers.len()) {
-                let data = mem::replace(&mut self.buffer, self.stash.borrow_mut().pop().unwrap_or(Vec::new()));
-                if index < observers.len() - 1 { self.buffer.push_all(&data); }
-                observers[index].give(data);
-            }
-        }
+        if self.buffer.len() > self.limit { self.flush_buffer(); }
     }
     #[inline(always)] fn shut(&mut self, time: &T) {
-        if self.buffer.len() > 0 {
-            let mut observers = self.shared.borrow_mut();
-            for index in (0..observers.len()) {
-                let data = mem::replace(&mut self.buffer, self.stash.borrow_mut().pop().unwrap_or(Vec::new()));
-                if index < observers.len() - 1 { self.buffer.push_all(&data); }
-                observers[index].give(data);
-            }
-        }
-
-        for observer in self.shared.borrow_mut().iter_mut() {
-            observer.shut(time);
-        }
+        if self.buffer.len() > 0 { self.flush_buffer(); }
+        for observer in self.shared.borrow_mut().iter_mut() { observer.shut(time); }
     }
 }
 
 impl<T: Timestamp, D: Data> OutputPort<T, D> {
     pub fn new() -> OutputPort<T, D> {
-        let limit = 256;
+        let limit = 256;    // TODO : Used to be a parameter, but not clear that the user should
+                            // TODO : need to know the right value here. Think a bit harder...
         OutputPort {
             limit:  limit,
             buffer: Vec::with_capacity(limit),
@@ -102,6 +50,16 @@ impl<T: Timestamp, D: Data> OutputPort<T, D> {
     }
     pub fn add_observer<O: Observer<Time=T, Data=D>+'static>(&self, observer: O) {
         self.shared.borrow_mut().push(Box::new(OutputPortFlattener::new(observer, self.stash.clone())));
+    }
+
+    fn flush_buffer(&mut self) {
+        let mut observers = self.shared.borrow_mut();
+        for index in (0..observers.len()) {
+            let data = mem::replace(&mut self.buffer, self.stash.borrow_mut().pop().unwrap_or(Vec::new()));
+            if index < observers.len() - 1 { println!("cloning data"); self.buffer.push_all(&data); }
+            observers[index].give(data);
+        }
+        self.buffer.clear(); // in case observers.len() == 0
     }
 }
 
@@ -137,7 +95,9 @@ impl<O: Observer> Observer for OutputPortFlattener<O> {
     type Time = O::Time;
     type Data = Vec<O::Data>;
     #[inline(always)] fn open(&mut self, time: &O::Time) -> () { self.observer.open(time); }
-    #[inline(always)] fn show(&mut self, _data: &Vec<O::Data>) -> () { panic!("something horribly wrong!") }
+    #[inline(always)] fn show(&mut self, _data: &Vec<O::Data>) -> () {
+        for datum in _data { self.observer.show(datum); }
+    }
     #[inline(always)] fn give(&mut self, mut data:  Vec<O::Data>) -> () {
         for datum in data.drain() { self.observer.give(datum); }
         self.stash.borrow_mut().push(data);
