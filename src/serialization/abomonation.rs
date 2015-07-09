d8c41f64440d5f5317e1287ff0509fcb1558e8ee
Frank McSherry <fmcsherry@me.com>
2015-07-09 10:21:24 +0200

    updated for new abomonation interface

--- src/serialization/abomonation.rs
+++ src/serialization/abomonation.rs
@@ -4,10 +4,10 @@ use abomonation::{Abomonation, encode, decode};
 
 impl<T: Abomonation+Clone> Serializable for T {
     fn encode(typed: Self, bytes: &mut Vec<u8>) {
-        encode(&vec![typed], bytes);
+        encode(&typed, bytes);
     }
     fn decode(bytes: &mut [u8]) -> Result<Self, &mut [u8]> {
-        let result = if let Ok(result) = decode::<T>(bytes) { Ok(result[0].clone()) } else { Err(()) };
+        let result = if let Ok(result) = decode::<T>(bytes) { Ok(result.clone()) } else { Err(()) };
         if let Ok(result) = result { Ok(result) } else { Err (bytes) }
     }
 }
