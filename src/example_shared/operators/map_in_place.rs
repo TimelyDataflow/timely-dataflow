42a20d014dfdd5594c1ae0198a4584e884b936ab
Frank McSherry <fmcsherry@me.com>
2015-07-23 18:08:46 +0200

    map_in_place updates records without copying

---
+++ src/example_shared/operators/map_in_place.rs
@@ -0,0 +1,20 @@
+use communication::*;
+use communication::pact::Pipeline;
+
+use example_shared::*;
+use example_shared::operators::unary::UnaryStreamExt;
+
+pub trait MapInPlaceExt<G: GraphBuilder, D: Data> {
+    fn map_in_place<L: Fn(&mut D)+'static>(&self, logic: L) -> Stream<G, D>;
+}
+
+impl<G: GraphBuilder, D: Data> MapInPlaceExt<G, D> for Stream<G, D> {
+    fn map_in_place<L: Fn(&mut D)+'static>(&self, logic: L) -> Stream<G, D> {
+        self.unary_stream(Pipeline, format!("MapInPlace"), move |input, output| {
+            while let Some((time, data)) = input.pull() {
+                for datum in data.iter_mut() { logic(datum); }
+                output.give_vector_at(&time, data);
+            }
+        })
+    }
+}
