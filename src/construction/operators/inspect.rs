658810931705715d293e333fe4ef3a3efbffacb4
Frank McSherry <fmcsherry@me.com>
2015-07-30 11:09:56 +0200

    correct generic params for unified trait

--- src/construction/operators/inspect.rs
+++ src/construction/operators/inspect.rs
@@ -3,12 +3,12 @@ use communication::pact::Pipeline;
 use construction::{Stream, GraphBuilder};
 use construction::operators::unary::UnaryStreamExt;
 
-pub trait InspectExt<D: Data> {
+pub trait InspectExt<G: GraphBuilder, D: Data> {
     fn inspect<F: FnMut(&D)+'static>(&self, func: F) -> Self;
     fn inspect_batch<F: FnMut(&G::Timestamp, &[D])+'static>(&self, mut func: F) -> Self;
 }
 
-impl<G: GraphBuilder, D: Data> InspectExt<D> for Stream<G, D> {
+impl<G: GraphBuilder, D: Data> InspectExt<G, D> for Stream<G, D> {
     fn inspect<F: FnMut(&D)+'static>(&self, mut func: F) -> Stream<G, D> {
         self.unary_stream(Pipeline, "Inspect", move |input, output| {
             while let Some((time, data)) = input.pull() {
@@ -17,9 +17,9 @@ impl<G: GraphBuilder, D: Data> InspectExt<D> for Stream<G, D> {
             }
         })
     }
-    
+
     fn inspect_batch<F: FnMut(&G::Timestamp, &[D])+'static>(&self, mut func: F) -> Stream<G, D> {
-        self.unary_stream(Pipeline, "Inspect", move |input, output| {
+        self.unary_stream(Pipeline, "InspectBatch", move |input, output| {
             while let Some((time, data)) = input.pull() {
                 func(&time, &data[..]);
                 output.session(time).give_message(data);
