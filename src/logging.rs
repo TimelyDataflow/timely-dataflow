32ea4d59d344498f56bca09bc3ece6b37d7cc926
Frank McSherry <fmcsherry@me.com>
2015-10-22 14:10:25 +0200

    logging now writes Events to arbitrary streams

--- src/logging.rs
+++ src/logging.rs
@@ -3,21 +3,20 @@
 extern crate time;
 
 use std::cell::RefCell;
+use std::io::Write;
+use std::fs::File;
 
 use ::Data;
 
-// use dataflow::operators::input::{Input, Handle};
-// use dataflow::operators::inspect::Inspect;
-
 use timely_communication::Allocate;
+use ::progress::timestamp::RootTimestamp;
+use ::progress::nested::product::Product;
+
 use dataflow::scopes::root::Root;
 use dataflow::Scope;
-
-use std::io::Write;
+use dataflow::operators::capture::{EventWriter, Event, EventPusher};
 
 use abomonation::Abomonation;
-
-// use drain::DrainExt;
 
 /// Logs `record` in `logger` if logging is enabled.
 pub fn log<T: Logger>(logger: &'static ::std::thread::LocalKey<T>, record: T::Record) {
@@ -36,51 +35,13 @@ pub trait Logger {
     fn flush(&self);
 }
 
-// /// Logs records to a timely stream.
-// pub struct TimelyLogger<T: Data> {
-//     buffer: RefCell<Vec<(T,u64)>>,
-//     double: RefCell<Vec<(T,u64)>>,
-//     stream: RefCell<Option<Handle<u64, (T,u64)>>>,
-// }
-//
-// impl<T: Data> Logger for TimelyLogger<T> {
-//     type Record = T;
-//     #[inline]
-//     fn log(&self, record: T) {
-//         self.buffer.borrow_mut().push((record, time::precise_time_ns()));
-//     }
-//     fn flush(&self) {
-//         ::std::mem::swap(&mut *self.buffer.borrow_mut(), &mut *self.double.borrow_mut());
-//         let mut queue = self.double.borrow_mut();
-//         let mut temp = self.stream.borrow_mut();
-//         let mut input = temp.as_mut().unwrap();
-//         for item in queue.drain_temp() {
-//             input.send(item);
-//         }
-//         input.advance_to(time::precise_time_ns());
-//     }
-// }
-//
-// impl<T: Data> TimelyLogger<T> {
-//     fn new() -> TimelyLogger<T> {
-//         TimelyLogger {
-//             buffer: RefCell::new(Vec::new()),
-//             double: RefCell::new(Vec::new()),
-//             stream: RefCell::new(None),
-//         }
-//     }
-//     fn set(&self, input: Handle<u64, (T,u64)>) {
-//         *self.stream.borrow_mut() = Some(input);
-//     }
-// }
-
-pub struct FileLogger<T: Data> {
+pub struct EventStreamLogger<T: Data, S: Write> {
     buffer: RefCell<Vec<(T,u64)>>,
-    bytes: RefCell<Vec<u8>>,
-    file: RefCell<Option<::std::fs::File>>,
+    stream: RefCell<Option<EventWriter<Product<RootTimestamp, u64>, (T,u64), S>>>,
+    last_time: RefCell<u64>,
 }
 
-impl<T: Data> Logger for FileLogger<T> {
+impl<T: Data, S: Write> Logger for EventStreamLogger<T, S> {
     type Record = T;
     #[inline]
     fn log(&self, record: T) {
@@ -88,10 +49,11 @@ impl<T: Data> Logger for FileLogger<T> {
     }
     fn flush(&self) {
         if self.buffer.borrow().len() > 0 {
-            if let Some(ref mut file) = *self.file.borrow_mut() {
-                unsafe { ::abomonation::encode(&(*self.buffer.borrow()), &mut (*self.bytes.borrow_mut())); }
-                file.write_all(&self.bytes.borrow()[..]).unwrap();
-                self.bytes.borrow_mut().clear();
+            if let Some(ref mut writer) = *self.stream.borrow_mut() {
+                let time = time::precise_time_ns();
+                writer.push(Event::Messages(RootTimestamp::new(time), self.buffer.borrow().clone()));
+                writer.push(Event::Progress(vec![(RootTimestamp::new(*self.last_time.borrow()),-1), (RootTimestamp::new(time), 1)]));
+                *self.last_time.borrow_mut() = time;
                 self.buffer.borrow_mut().clear();
             }
             else {
@@ -101,65 +63,45 @@ impl<T: Data> Logger for FileLogger<T> {
     }
 }
 
-impl<T: Data> FileLogger<T> {
-    fn new() -> FileLogger<T> {
-        FileLogger {
+impl<T: Data, S: Write> EventStreamLogger<T, S> {
+    fn new() -> EventStreamLogger<T, S> {
+        EventStreamLogger {
             buffer: RefCell::new(Vec::new()),
-            bytes: RefCell::new(Vec::new()),
-            file: RefCell::new(None),
+            stream: RefCell::new(None),
+            last_time: RefCell::new(0),
         }
     }
-    fn set(&self, filename: String) {
-        let fname = filename.clone();
-        *self.file.borrow_mut() = Some(::std::fs::File::create(filename).ok().expect(&fname));
+    fn set(&self, stream: S) {
+        *self.stream.borrow_mut() = Some(EventWriter::new(stream));
     }
 }
 
-// pub struct ConsoleLogger<T: Data> {
-//     type Record = T;
-//     #[inline]
-//     fn log(&self, record: T) {
-//         println!("{}:\t{:?}", time::precise_time_ns(), record);
-//     }
-// }
+impl<T: Data, S: Write> Drop for EventStreamLogger<T, S> {
+    fn drop(&mut self) {
+        if self.buffer.borrow().len() > 0 {
+            if let Some(ref mut writer) = *self.stream.borrow_mut() {
+                let time = time::precise_time_ns();
+                writer.push(Event::Messages(RootTimestamp::new(time), self.buffer.borrow().clone()));
+                writer.push(Event::Progress(vec![(RootTimestamp::new(*self.last_time.borrow()),-1)]));
+            }
+            else {
+                panic!("logging file not initialized!!!!");
+            }
+        }
+    }
+}
 
 
 /// Initializes logging; called as part of `Root` initialization.
 pub fn initialize<A: Allocate>(root: &mut Root<A>) {
 
-    OPERATES.with(|x| x.set(format!("logs/operates-{}.abom", root.index())));
-    CHANNELS.with(|x| x.set(format!("logs/channels-{}.abom", root.index())));
-    MESSAGES.with(|x| x.set(format!("logs/messages-{}.abom", root.index())));
-    PROGRESS.with(|x| x.set(format!("logs/progress-{}.abom", root.index())));
-    SCHEDULE.with(|x| x.set(format!("logs/schedule-{}.abom", root.index())));
-    GUARDED_MESSAGE.with(|x| x.set(format!("logs/guarded_message-{}.abom", root.index())));
-    GUARDED_PROGRESS.with(|x| x.set(format!("logs/guarded_progress-{}.abom", root.index())));
-
-    // root.scoped(move |scope| {
-    //     let (input_o, stream_o) = scope.new_input();
-    //     let (input_c, stream_c) = scope.new_input();
-    //     let (input_p, stream_p) = scope.new_input();
-    //     let (input_m, stream_m) = scope.new_input();
-    //     let (input_s, stream_s) = scope.new_input();
-    //     let (input_gm, stream_gm) = scope.new_input();
-    //     let (input_gp, stream_gp) = scope.new_input();
-    //
-    //     stream_o.inspect_batch(|t, x| { println!("OPERATES at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_c.inspect_batch(|t, x| { println!("CHANNELS at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_m.inspect_batch(|t, x| { println!("MESSAGES at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_p.inspect_batch(|t, x| { println!("PROGRESS at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_s.inspect_batch(|t, x| { println!("SCHEDULE at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_gm.inspect_batch(|t, x| { println!("GUARDED_MESSAGE at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //     stream_gp.inspect_batch(|t, x| { println!("GUARDED_PROGRESS at {:?}:", t); for elem in x { println!("  {:?}", elem) } });
-    //
-    //     OPERATES.with(|x| x.set(input_o));
-    //     CHANNELS.with(|x| x.set(input_c));
-    //     MESSAGES.with(|x| x.set(input_m));
-    //     PROGRESS.with(|x| x.set(input_p));
-    //     SCHEDULE.with(|x| x.set(input_s));
-    //     GUARDED_MESSAGE.with(|x| x.set(input_gm));
-    //     GUARDED_PROGRESS.with(|x| x.set(input_gp));
-    // });
+    OPERATES.with(|x| x.set(File::create(format!("logs/operates-{}.abom", root.index())).unwrap()));
+    CHANNELS.with(|x| x.set(File::create(format!("logs/channels-{}.abom", root.index())).unwrap()));
+    MESSAGES.with(|x| x.set(File::create(format!("logs/messages-{}.abom", root.index())).unwrap()));
+    PROGRESS.with(|x| x.set(File::create(format!("logs/progress-{}.abom", root.index())).unwrap()));
+    SCHEDULE.with(|x| x.set(File::create(format!("logs/schedule-{}.abom", root.index())).unwrap()));
+    GUARDED_MESSAGE.with(|x| x.set(File::create(format!("logs/guarded_message-{}.abom", root.index())).unwrap()));
+    GUARDED_PROGRESS.with(|x| x.set(File::create(format!("logs/guarded_progress-{}.abom", root.index())).unwrap()));
 }
 
 /// Flushes logs; called by `Root::step`.
@@ -173,21 +115,13 @@ pub fn flush_logs() {
     GUARDED_PROGRESS.with(|x| x.flush());
 }
 
-// thread_local!(pub static OPERATES: TimelyLogger<OperatesEvent> = TimelyLogger::new());
-// thread_local!(pub static CHANNELS: TimelyLogger<ChannelsEvent> = TimelyLogger::new());
-// thread_local!(pub static PROGRESS: TimelyLogger<ProgressEvent> = TimelyLogger::new());
-// thread_local!(pub static MESSAGES: TimelyLogger<MessagesEvent> = TimelyLogger::new());
-// thread_local!(pub static SCHEDULE: TimelyLogger<ScheduleEvent> = TimelyLogger::new());
-// thread_local!(pub static GUARDED_MESSAGE: TimelyLogger<bool> = TimelyLogger::new());
-// thread_local!(pub static GUARDED_PROGRESS: TimelyLogger<bool> = TimelyLogger::new());
-
-thread_local!(pub static OPERATES: FileLogger<OperatesEvent> = FileLogger::new());
-thread_local!(pub static CHANNELS: FileLogger<ChannelsEvent> = FileLogger::new());
-thread_local!(pub static PROGRESS: FileLogger<ProgressEvent> = FileLogger::new());
-thread_local!(pub static MESSAGES: FileLogger<MessagesEvent> = FileLogger::new());
-thread_local!(pub static SCHEDULE: FileLogger<ScheduleEvent> = FileLogger::new());
-thread_local!(pub static GUARDED_MESSAGE: FileLogger<bool> = FileLogger::new());
-thread_local!(pub static GUARDED_PROGRESS: FileLogger<bool> = FileLogger::new());
+thread_local!(pub static OPERATES: EventStreamLogger<OperatesEvent, File> = EventStreamLogger::new());
+thread_local!(pub static CHANNELS: EventStreamLogger<ChannelsEvent, File> = EventStreamLogger::new());
+thread_local!(pub static PROGRESS: EventStreamLogger<ProgressEvent, File> = EventStreamLogger::new());
+thread_local!(pub static MESSAGES: EventStreamLogger<MessagesEvent, File> = EventStreamLogger::new());
+thread_local!(pub static SCHEDULE: EventStreamLogger<ScheduleEvent, File> = EventStreamLogger::new());
+thread_local!(pub static GUARDED_MESSAGE: EventStreamLogger<bool, File> = EventStreamLogger::new());
+thread_local!(pub static GUARDED_PROGRESS: EventStreamLogger<bool, File> = EventStreamLogger::new());
 
 #[derive(Debug, Clone)]
 /// The creation of an `Operate` implementor.
