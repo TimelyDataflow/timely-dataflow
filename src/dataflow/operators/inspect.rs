55a78510003df20f5d1a8d312f44641c7a8f09ef
Frank McSherry <fmcsherry@me.com>
2016-04-05 11:19:08 +0200

    added examples/doctests for inspect/inspect_batch

--- src/dataflow/operators/inspect.rs
+++ src/dataflow/operators/inspect.rs
@@ -7,13 +7,34 @@ use dataflow::operators::unary::Unary;
 
 /// Methods to inspect records and batches of records on a stream.
 pub trait Inspect<G: Scope, D: Data> {
-    /// Creates a dataflow operator that calls the supplied function on each record.
+    /// Runs a supplied closure on each observed data element.
+    ///
+    /// #Examples
+    /// ```
+    /// use timely::dataflow::operators::{ToStream, Map, Inspect};
+    ///
+    /// timely::example(|scope| {
+    ///     (0..10).to_stream(scope)
+    ///            .inspect(|x| println!("seen: {:?}", x));
+    /// });
+    /// ```
     fn inspect<F: FnMut(&D)+'static>(&self, func: F) -> Self;
-    /// Creates a dataflow operator that calls the supplied function on each timestamp and batch of records.
+    /// Runs a supplied closure on each observed data batch (time and data slice).
+    ///
+    /// #Examples
+    /// ```
+    /// use timely::dataflow::operators::{ToStream, Map, Inspect};
+    ///
+    /// timely::example(|scope| {
+    ///     (0..10).to_stream(scope)
+    ///            .inspect_batch(|t,xs| println!("seen at: {:?}\t{:?} records", t, xs.len()));
+    /// });
+    /// ```
     fn inspect_batch<F: FnMut(&G::Timestamp, &[D])+'static>(&self, mut func: F) -> Self;
 }
 
 impl<G: Scope, D: Data> Inspect<G, D> for Stream<G, D> {
+
     fn inspect<F: FnMut(&D)+'static>(&self, mut func: F) -> Stream<G, D> {
         self.unary_stream(Pipeline, "Inspect", move |input, output| {
             input.for_each(|time, data| {
