261411078e32782dad3c459cd513af6b380ff1cf
Frank McSherry <fmcsherry@me.com>
2016-05-30 10:23:41 +0200

    documentation

--- src/dataflow/channels/pushers/buffer.rs
+++ src/dataflow/channels/pushers/buffer.rs
@@ -1,6 +1,10 @@
 use dataflow::channels::Content;
 use timely_communication::Push;
 
+/// Buffers data sent at the same time, for efficient communication.
+///
+/// The `Buffer` type should be used by calling `session` with a time, which checks whether
+/// data must be flushed and creates a `Session` object which allows sending at the given time. 
 pub struct Buffer<T, D, P: Push<(T, Content<D>)>> {
     time: Option<T>,  // the currently open time, if it is open
     buffer: Vec<D>,   // a buffer for records, to send at self.time
@@ -9,6 +13,7 @@ pub struct Buffer<T, D, P: Push<(T, Content<D>)>> {
 
 impl<T, D, P: Push<(T, Content<D>)>> Buffer<T, D, P> where T: Eq+Clone {
 
+    /// Creates a new `Buffer`.
     pub fn new(pusher: P) -> Buffer<T, D, P> {
         Buffer {
             time: None,
@@ -17,15 +22,19 @@ impl<T, D, P: Push<(T, Content<D>)>> Buffer<T, D, P> where T: Eq+Clone {
         }
     }
 
-    /// Returns a Session, which accepts data to send at the associated time
+    /// Returns a `Session`, which accepts data to send at the associated time
     pub fn session(&mut self, time: &T) -> Session<T, D, P> {
         if let Some(true) = self.time.as_ref().map(|x| x != time) { self.flush(); }
         self.time = Some(time.clone());
         Session { buffer: self }
     }
 
+    /// Returns a reference to the inner `P: Push` type.
+    ///
+    /// This is currently used internally, and should not be used without some care.
     pub fn inner(&mut self) -> &mut P { &mut self.pusher }
 
+    /// Flushes all data and pushes a `None` to `self.pusher`, indicating a flush.
     pub fn cease(&mut self) {
         self.flush();
         self.pusher.push(&mut None);
@@ -39,6 +48,7 @@ impl<T, D, P: Push<(T, Content<D>)>> Buffer<T, D, P> where T: Eq+Clone {
         }
     }
 
+    // internal method for use by `Session`.
     fn give(&mut self, data: D) {
         self.buffer.push(data);
         // assert!(self.buffer.capacity() == Message::<O::Data>::default_length());
@@ -47,7 +57,7 @@ impl<T, D, P: Push<(T, Content<D>)>> Buffer<T, D, P> where T: Eq+Clone {
         }
     }
 
-    /// Gives an entire message at a specific time.
+    // Gives an entire message at a specific time.
     fn give_content(&mut self, content: &mut Content<D>) {
         // flush to ensure fifo-ness
         if !self.buffer.is_empty() {
@@ -66,22 +76,33 @@ impl<T, D, P: Push<(T, Content<D>)>> Buffer<T, D, P> where T: Eq+Clone {
 }
 
 
-
+/// An output session for sending records at a specified time.
+///
+/// The `Session` struct provides the user-facing interface to an operator output, namely
+/// the `Buffer` type. A `Session` wraps a session of output at a specified time, and 
+/// avoids what would otherwise be a constant cost of checking timestamp equality.
 pub struct Session<'a, T, D, P: Push<(T, Content<D>)>+'a> where T: Eq+Clone+'a, D: 'a {
     buffer: &'a mut Buffer<T, D, P>,
 }
 
 impl<'a, T, D, P: Push<(T, Content<D>)>+'a> Session<'a, T, D, P>  where T: Eq+Clone+'a, D: 'a {
+    /// Provides one record at the time specified by the `Session`.
     #[inline(always)]
     pub fn give(&mut self, data: D) {
         self.buffer.give(data);
     }
+    /// Provides an iterator of records at the time specified by the `Session`.
     #[inline(always)]
     pub fn give_iterator<I: Iterator<Item=D>>(&mut self, iter: I) {
         for item in iter {
             self.give(item);
         }
     }
+    /// Provides a fully formed `Content<D>` message for senders which can use this type.
+    ///
+    /// The `Content` type is the backing memory for communication in timely, and it can
+    /// often be more efficient to re-use this memory rather than have timely allocate 
+    /// new backing memory.
     #[inline(always)]
     pub fn give_content(&mut self, message: &mut Content<D>) {
         if message.len() > 0 {
