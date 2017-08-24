149abf9ac3855787b8a01ac325da005282a1d8c4
Frank McSherry <fmcsherry@me.com>
2017-08-24 17:38:23 -0400

    added barrier sync test

---
+++ tests/barrier.rs
@@ -0,0 +1,32 @@
+extern crate timely;
+
+use timely::dataflow::channels::pact::Pipeline;
+use timely::progress::timestamp::RootTimestamp;
+
+use timely::dataflow::operators::{LoopVariable, ConnectLoop};
+use timely::dataflow::operators::generic::unary::Unary;
+
+#[test]
+fn barrier_sync() {
+    timely::example(|scope| {
+        let (handle, stream) = scope.loop_variable::<u64>(100, 1);
+        stream.unary_notify(
+            Pipeline,
+            "Barrier",
+            vec![RootTimestamp::new(0)],
+            move |_, _, notificator| {
+                let mut once = true;
+                while let Some((cap, _count)) = notificator.next() {
+                    assert!(once);
+                    once = false;
+                    let mut time = cap.time().clone();
+                    time.inner += 1;
+                    if time.inner < 100 {
+                        notificator.notify_at(cap.delayed(&time));
+                    }
+                }
+            }
+        )
+        .connect_loop(handle);
+    }); // asserts error-free execution;
+}
