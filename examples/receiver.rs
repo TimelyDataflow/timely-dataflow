7870d40ae137a10c37b55f932057f7a510c0affa
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2017-12-15 11:09:30 +0100

    examples: Receiver cleanup
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- examples/receiver.rs
+++ examples/receiver.rs
@@ -4,10 +4,9 @@ extern crate timely_logging;
 
 use std::net::TcpListener;
 use std::io;
-use std::io::Read;
 
 use timely_logging::{CommsSetup, EventsSetup, Event as LogEvent, CommsEvent};
-use timely::dataflow::operators::capture::{Event, EventReader};
+use timely::dataflow::operators::capture::EventReader;
 use timely::progress::nested::product::Product;
 use timely::progress::timestamp::RootTimestamp;
 use timely::dataflow::operators::capture::event::EventIterator;
@@ -23,13 +22,13 @@ fn main() {
     let mut i = 0;
     for socket in listener.incoming() {
         i += 1;
-        let thread = ::std::thread::spawn(move || {
-            let mut socket = io::BufReader::new(socket.unwrap());
+        let _thread = ::std::thread::spawn(move || {
+            let socket = io::BufReader::new(socket.unwrap());
             if communication {
                 let mut reader = EventReader::<Product<RootTimestamp, u64>, (u64, CommsSetup, CommsEvent), _>::new(socket);
                 println!("communication");
                 loop {
-                    if let Some(e) = reader.next() {
+                    if let Some(_e) = reader.next() {
                         println!("{} →", i);
                     }
                 }
@@ -37,7 +36,7 @@ fn main() {
                 let mut reader = EventReader::<Product<RootTimestamp, u64>, (u64, EventsSetup, LogEvent), _>::new(socket);
                 println!("log");
                 loop {
-                    if let Some(e) = reader.next() {
+                    if let Some(_e) = reader.next() {
                         println!("{} →", i);
                     }
                 }
