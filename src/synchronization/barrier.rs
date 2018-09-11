8187ebb2499d0e18cf6a340e2db4cb7b3003ed1c
Frank McSherry <fmcsherry@me.com>
2018-09-11 13:35:42 +0200

    synchcronization

---
+++ src/synchronization/barrier.rs
@@ -0,0 +1,41 @@
+//! Barrier synchronization.
+
+use ::communication::Allocate;
+use progress::timestamp::RootTimestamp;
+use progress::nested::product::Product;
+use dataflow::{InputHandle, ProbeHandle};
+use dataflow::scopes::root::Root;
+
+/// A re-usable barrier synchronization mechanism.
+pub struct Barrier<A: Allocate> {
+    round: usize,
+    input: InputHandle<usize, ()>,
+    probe: ProbeHandle<Product<RootTimestamp, usize>>,
+    worker: Root<A>,
+}
+
+impl<A: Allocate> Barrier<A> {
+
+    /// Allocates a new barrier.
+    pub fn new(worker: &mut Root<A>) -> Self {
+        use dataflow::operators::{Input, Probe};
+        let (input, probe) = worker.dataflow(|scope| {
+            let (handle, stream) = scope.new_input::<()>();
+            (handle, stream.probe())
+        });
+        Barrier { round: 0, input, probe, worker: worker.clone() }
+    }
+
+    /// Blocks until all other workers have reached this barrier.
+    ///
+    /// This method does *not* block dataflow execution, which continues to execute while
+    /// we await the arrival of the other workers.
+    pub fn wait(&mut self) {
+        self.round += 1;
+        self.input.advance_to(self.round);
+        while self.probe.less_than(self.input.time()) {
+            self.worker.step();
+        }
+    }
+}
+
