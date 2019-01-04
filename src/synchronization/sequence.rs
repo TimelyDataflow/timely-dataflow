2d6527a04c39de70c743484d2e75f2c8ffd01c83
niko <me@nikolasgoebel.com>
2019-01-04 21:16:34 +0100

    Improve Sequencer scheduling (#215)

--- src/synchronization/sequence.rs
+++ src/synchronization/sequence.rs
@@ -5,18 +5,32 @@ use std::cell::RefCell;
 use std::time::{Instant, Duration};
 use std::collections::VecDeque;
 
-use ::{communication::Allocate, ExchangeData};
+use ::{communication::Allocate, ExchangeData, PartialOrder};
 use ::scheduling::Scheduler;
 use worker::Worker;
 use dataflow::channels::pact::Exchange;
 use dataflow::operators::generic::operator::source;
 use dataflow::operators::generic::operator::Operator;
+use scheduling::activate::Activator;
+
+struct CatchupActivator {
+    pub catchup_until: Option<Duration>,
+    activator: Activator,
+}
+
+impl CatchupActivator {
+    pub fn activate(&self) {
+        self.activator.activate();
+    }
+}
+
 
 /// Orders elements inserted across all workers.
 ///
 /// A Sequencer allows each worker to insert into a consistent ordered sequence
 /// that is seen by all workers in the same order.
 pub struct Sequencer<T> {
+    activator: Rc<RefCell<Option<CatchupActivator>>>,
     send: Rc<RefCell<VecDeque<T>>>, // proposed items.
     recv: Rc<RefCell<VecDeque<T>>>, // sequenced items.
 }
@@ -35,6 +49,10 @@ impl<T: Ord+ExchangeData> Sequencer<T> {
         let send_weak = Rc::downgrade(&send);
         let recv_weak = Rc::downgrade(&recv);
 
+        let activator = Rc::new(RefCell::new(None));
+        let activator_weak = Rc::downgrade(&activator);
+        let activator_weak_sink = Rc::downgrade(&activator);
+
         // build a dataflow used to serialize and circulate commands
         worker.dataflow::<Duration,_,_>(move |dataflow| {
 
@@ -43,11 +61,16 @@ impl<T: Ord+ExchangeData> Sequencer<T> {
             let mut vector = Vec::new();
 
             let scope = dataflow.clone();
-
+        
             // a source that attempts to pull from `recv` and produce commands for everyone
             source(dataflow, "SequenceInput", move |capability, info| {
 
-                let activator = scope.activator_for(&info.address[..]);
+                activator_weak.upgrade()
+                    .unwrap()
+                    .replace(Some(CatchupActivator {
+                        activator: scope.activator_for(&info.address[..]),
+                        catchup_until: None,
+                    }));
 
                 // so we can drop, if input queue vanishes.
                 let mut capability = Some(capability);
@@ -72,7 +95,21 @@ impl<T: Ord+ExchangeData> Sequencer<T> {
                             }
                         }
 
-                        activator.activate();
+                        if let Some(activator) = activator_weak.upgrade() {
+
+                            let mut activator_borrow = activator.borrow_mut();
+                            let mut activator = activator_borrow.as_mut().unwrap();
+                            
+                            if let Some(t) = activator.catchup_until {
+                                if capability.time().less_than(&t) {
+                                    activator.activate();
+                                } else {
+                                    activator.catchup_until = None;
+                                }
+                            }
+                        } else {
+                            panic!("can't upgrade activator");
+                        }
                     }
                     else {
                         capability = None;
@@ -94,6 +131,18 @@ impl<T: Ord+ExchangeData> Sequencer<T> {
 
                     recvd.sort();
 
+                    if let Some(last) = recvd.last() {
+                        if let Some(activator) = activator_weak_sink.upgrade() {
+                            let mut activator_borrow = activator.borrow_mut();
+                            let mut activator = activator_borrow.as_mut().unwrap();
+
+                            activator.catchup_until = Some(last.0);
+                            activator.activate();
+                        } else {
+                            panic!("can't upgrade activator");
+                        }
+                    }
+
                     // determine how many (which) elements to read from `recvd`.
                     let count = recvd.iter().filter(|&(ref time, _)| !input.frontier().less_equal(time)).count();
                     let iter = recvd.drain(..count);
@@ -105,12 +154,13 @@ impl<T: Ord+ExchangeData> Sequencer<T> {
             );
         });
 
-        Sequencer { send, recv, }
+        Sequencer { activator, send, recv, }
     }
 
     /// Adds an element to the shared log.
     pub fn push(&mut self, element: T) {
         self.send.borrow_mut().push_back(element);
+        self.activator.borrow_mut().as_mut().unwrap().activate();
     }
 
     /// Reads the next element from the shared log.
