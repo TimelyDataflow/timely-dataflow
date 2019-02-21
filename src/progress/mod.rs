a1e2ea20744e064b07d10fe9a93df9db6806a349
Frank McSherry <fmcsherry@me.com>
2019-02-21 09:25:52 +0100

    lift reachability and subgraph

--- src/progress/mod.rs
+++ src/progress/mod.rs
@@ -1,17 +1,18 @@
 //! Progress tracking mechanisms to support notification in timely dataflow
 
 pub use self::operate::Operate;
-pub use self::nested::{Subgraph, SubgraphBuilder};
+pub use self::subgraph::{Subgraph, SubgraphBuilder};
 pub use self::timestamp::{Timestamp, PathSummary};
 pub use self::change_batch::ChangeBatch;
 pub use self::frontier::Antichain;
 
 pub mod change_batch;
 pub mod frontier;
-pub mod nested;
 pub mod timestamp;
 pub mod operate;
 pub mod broadcast;
+pub mod reachability;
+pub mod subgraph;
 
 /// A timely dataflow location.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Abomonation, Serialize, Deserialize)]
--- src/progress/nested/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Coordination of progress information between a scope-as-operator and its children operators.
-
-pub use self::subgraph::{Subgraph, SubgraphBuilder};
-
-// pub mod pointstamp_counter;
-pub mod subgraph;
-pub mod reachability;
