81ab002b6dee935a692d42d67730ad3af5b85f27
Frank McSherry <fmcsherry@me.com>
2019-12-12 14:36:05 -0500

    delete deprecated files

--- timely/src/dataflow/operators/generic/mod.rs
+++ timely/src/dataflow/operators/generic/mod.rs
@@ -1,7 +1,5 @@
 //! Generic operators defined by user-provided closures.
 
-// pub mod unary;
-// pub mod binary;
 pub mod operator;
 pub mod builder_rc;
 pub mod builder_raw;
@@ -13,7 +11,5 @@ mod operator_info;
 pub use self::handles::{InputHandle, FrontieredInputHandle, OutputHandle, OutputWrapper};
 pub use self::notificator::{Notificator, FrontierNotificator};
 
-// pub use self::unary::Unary;
-// pub use self::binary::Binary;
 pub use self::operator::{Operator, source};
 pub use self::operator_info::OperatorInfo;
