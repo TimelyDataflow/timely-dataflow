8ecced5afe6bf53617faedfed0f703e9c7179ddf
Moritz Hoffmann <antiguru@gmail.com>
2021-08-23 11:32:59 +0200

    exchange: Unify all exchange variants to generic impl
    Instead of defining distinct exchange pushers, make the default generic
    and provide necessary hooks for specialized exchange pushers.
    
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- timely/src/dataflow/channels/pushers/lazy_exchange.rs
+++ timely/src/dataflow/channels/pushers/lazy_exchange.rs
@@ -2,9 +2,10 @@
 
 use std::marker::PhantomData;
 
-use crate::{Data, ExchangeData};
+use crate::ExchangeData;
 use crate::communication::{Pull, Push};
 use crate::dataflow::channels::pact::{ParallelizationContract, LogPusher, LogPuller};
+use crate::dataflow::channels::pushers::exchange::{ExchangeBehavior, ExchangePusherGeneric};
 use crate::dataflow::channels::{Bundle, Message};
 use crate::logging::TimelyLogger as Logger;
 use crate::worker::AsWorker;
@@ -15,107 +16,31 @@ use crate::worker::AsWorker;
 /// tries to leave less allocations around. It does not preallocate a buffer for each pushee, but
 /// only allocates it once data is pushed. On flush, the allocation is passed to the pushee, and
 /// only what it is passed back is retained.
-pub struct LazyExchangePusher<T, D, P: Push<Bundle<T, D>>, H: FnMut(&T, &D) -> u64> {
-    pushers: Vec<P>,
-    buffers: Vec<Vec<D>>,
-    current: Option<T>,
-    hash_func: H,
-}
+pub struct LazyExchangeBehavior {}
 
-impl<T: Clone, D, P: Push<Bundle<T, D>>, H: FnMut(&T, &D)->u64> LazyExchangePusher<T, D, P, H> {
-    /// Allocates a new `Exchange` from a supplied set of pushers and a distribution function.
-    pub fn new(pushers: Vec<P>, key: H) -> LazyExchangePusher<T, D, P, H> {
-        let buffers = (0..pushers.len()).map(|_| vec![]).collect();
-        LazyExchangePusher {
-            pushers,
-            hash_func: key,
-            buffers,
-            current: None,
+impl<T, D> ExchangeBehavior<T, D> for LazyExchangeBehavior {
+    fn allocate() -> Vec<D> {
+        Vec::new()
+    }
+
+    fn check(buffer: &mut Vec<D>) {
+        if buffer.capacity() < Message::<T, D>::default_length() {
+            let to_reserve = Message::<T, D>::default_length() - buffer.capacity();
+            buffer.reserve(to_reserve);
         }
     }
-    #[inline]
-    fn flush(&mut self, index: usize) {
-        if !self.buffers[index].is_empty() {
-            if let Some(ref time) = self.current {
-                Message::push_at_no_allocation(&mut self.buffers[index], time.clone(), &mut self.pushers[index]);
-            }
-        }
+
+    fn flush<P: Push<Bundle<T, D>>>(buffer: &mut Vec<D>, time: T, pusher: &mut P) {
+        Message::push_at_no_allocation(buffer, time, pusher);
+    }
+
+    fn finalize(_buffer: &mut Vec<D>) {
+        // None
     }
 }
 
-impl<T: Eq+Data, D: Data, P: Push<Bundle<T, D>>, H: FnMut(&T, &D)->u64> Push<Bundle<T, D>> for LazyExchangePusher<T, D, P, H> {
-    fn push(&mut self, message: &mut Option<Bundle<T, D>>) {
-        // if only one pusher, no exchange
-        if self.pushers.len() == 1 {
-            self.pushers[0].push(message);
-        } else if let Some(message) = message {
-            let message = message.as_mut();
-            let time = &message.time;
-            let data = &mut message.data;
-
-            // if the time isn't right, flush everything.
-            if self.current.as_ref().map_or(false, |x| x != time) {
-                for index in 0..self.pushers.len() {
-                    self.flush(index);
-                }
-            }
-            self.current = Some(time.clone());
-
-            // if the number of pushers is a power of two, use a mask
-            if (self.pushers.len() & (self.pushers.len() - 1)) == 0 {
-                let mask = (self.pushers.len() - 1) as u64;
-                for datum in data.drain(..) {
-                    let index = (((self.hash_func)(time, &datum)) & mask) as usize;
-                    // Push at the target buffer, which might be without capacity, or preallocated
-                    self.buffers[index].push(datum);
-                    // We have reached the buffer's capacity
-                    if self.buffers[index].len() == self.buffers[index].capacity() {
-                        // If the buffer's capacity is below the default length, reallocate to match
-                        // the default length
-                        if self.buffers[index].capacity() < Message::<T, D>::default_length() {
-                            let to_reserve = Message::<T, D>::default_length() - self.buffers[index].capacity();
-                            self.buffers[index].reserve(to_reserve);
-                        } else {
-                            // Buffer is at capacity, flush
-                            self.flush(index);
-                            // Explicitly allocate a new buffer under the assumption that more data
-                            // will be sent to the pushee.
-                            if self.buffers[index].capacity() < Message::<T, D>::default_length() {
-                                let to_reserve =  Message::<T, D>::default_length() - self.buffers[index].capacity();
-                                self.buffers.reserve(to_reserve);
-                            }
-                        }
-                    }
-                }
-            } else {
-                // as a last resort, use mod (%)
-                for datum in data.drain(..) {
-                    let index = (((self.hash_func)(time, &datum)) % self.pushers.len() as u64) as usize;
-                    self.buffers[index].push(datum);
-                    // This code is duplicated from above, keep in sync!
-                    if self.buffers[index].len() == self.buffers[index].capacity() {
-                        if self.buffers[index].capacity() < Message::<T, D>::default_length() {
-                            let to_reserve = Message::<T, D>::default_length() - self.buffers[index].capacity();
-                            self.buffers[index].reserve(to_reserve);
-                        } else {
-                            self.flush(index);
-                            if self.buffers[index].capacity() < Message::<T, D>::default_length() {
-                                let to_reserve =  Message::<T, D>::default_length() - self.buffers[index].capacity();
-                                self.buffers.reserve(to_reserve);
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            // flush
-            for index in 0..self.pushers.len() {
-                self.flush(index);
-                self.pushers[index].push(&mut None);
-            }
-        }
-    }
-}
+/// Lazy exchange pusher definition
+pub type LazyExchangePusher<T, D, P, H> = ExchangePusherGeneric<T, D, P, H, LazyExchangeBehavior>;
 
 /// An exchange between multiple observers by data, backed by [LazyExchangePusher].
 pub struct LazyExchange<D, F> { hash_func: F, phantom: PhantomData<D> }
