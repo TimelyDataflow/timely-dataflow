915778aba88fa5eca1a667adeefad09dc74f54e5
Moritz Hoffmann <antiguru@gmail.com>
2021-10-29 13:15:26 +0200

    Promote non-preallocating exchange to default
    I did some measurements comparing the
    * current default,
    * a non-preallocating exchange
    * id. plus eagerly releasing exchange.
    
    The benchmark in the chain of commits does not report a significant change
    in performance for all variants. Erring on the side of caution, I'd propse
    to make the second variant, i.e. not pre-allocating, but retaining if push
    returned a buffer, the default exchange pact.
    
    Currently, all non-zerocopy allocators will not return an allocation on
    push, so in this case, the second and third variant are equivalent.
    
    # Why is there no change in performance?
    
    For single-recrod exchanges, this can be reasoned as follows. In the old
    approach, we'd have an allocated buffer, where we write a single element.
    On flush, the buffer would be sent to the pusher, which might or might not
    return a buffer. If it doesn't, we'd allocate a new one.
    
    In the new approach, we don't have a buffer to append to, so we'd first
    allocate one. On flush, we send the buffer downstream and maybe get one
    back. We don't eagerly allocate a new one if there is no buffer available.
    
    Both approaches allocate a new buffer if the pusher doesn't/didn't return
    one earlier. Only the order of allocations is changed. While in the old
    approach we send data and allocate, we allocate and send data in the new
    approach. On top of this, the new approach needs to pay a cost of checking
    the buffer's capacity for each record on the input, but I'd hope for the
    optimizer to sort this out.
    
    The same argument seems to hold for larger batch sizes.
    
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- experiments/benches/exchange_bench.rs
+++ experiments/benches/exchange_bench.rs
@@ -6,9 +6,6 @@ use std::time::{Duration, Instant};
 use criterion::black_box;
 use criterion::*;
 
-use timely::dataflow::channels::pact::{
-    Exchange as ExchangePact, NonRetainingExchange, ParallelizationContract,
-};
 use timely::dataflow::operators::{Exchange, Input, Probe};
 use timely::dataflow::InputHandle;
 use timely::{CommunicationConfig, Config, WorkerConfig};
@@ -43,22 +40,6 @@ fn bench(c: &mut Criterion) {
                             config,
                             params.batch,
                             iters,
-                            || ExchangePact::new(|&x| x as u64),
-                        ))
-                    })
-                },
-            );
-            group.bench_with_input(
-                BenchmarkId::new("NonRetaining", params.clone()),
-                &params,
-                move |b, params| {
-                    b.iter_custom(|iters| {
-                        let config = Config::process(params.threads);
-                        black_box(experiment_exchange(
-                            config,
-                            params.batch,
-                            iters,
-                            || NonRetainingExchange::new(|&x| x as u64),
                         ))
                     })
                 },
@@ -76,25 +57,6 @@ fn bench(c: &mut Criterion) {
                             config,
                             params.batch,
                             iters,
-                            ||ExchangePact::new(|&x| x as u64),
-                        ))
-                    })
-                },
-            );
-            group.bench_with_input(
-                BenchmarkId::new("NonRetainingZero", params.clone()),
-                &params,
-                move |b, params| {
-                    b.iter_custom(|iters| {
-                        let config = Config {
-                            communication: CommunicationConfig::ProcessBinary(params.threads),
-                            worker: WorkerConfig::default(),
-                        };
-                        black_box(experiment_exchange(
-                            config,
-                            params.batch,
-                            iters,
-                            ||NonRetainingExchange::new(|&x| x as u64),
                         ))
                     })
                 },
@@ -103,15 +65,14 @@ fn bench(c: &mut Criterion) {
     }
 }
 
-fn experiment_exchange<F: Fn() -> P + Sync + Send + 'static, P: ParallelizationContract<i32, u64>>(
+fn experiment_exchange(
     config: Config,
     batch: u64,
     rounds: u64,
-    pact: F,
 ) -> Duration {
     timely::execute(config, move |worker| {
         let mut input = InputHandle::new();
-        let probe = worker.dataflow(|scope| scope.input_from(&mut input).apply_pact(pact()).probe());
+        let probe = worker.dataflow(|scope| scope.input_from(&mut input).exchange(|x| *x).probe());
 
         let mut time = 0;
         let timer = Instant::now();
