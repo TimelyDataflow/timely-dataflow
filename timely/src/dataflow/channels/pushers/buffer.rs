TimelyDataflow/timely-dataflow

commit 84ff8bfe2d5bd1445d3994d27f3aba6fbb221dac

Container stream (without Allocation) (#426)
* Timely container abstraction

Introduces a container abstraction to enable generic data on dataflow
edges. A container describes its contents, has a length and capacity,
can be cleared. Containers implementing the PushPartitioned trait are
suitable for exchanging across Timely workers.

Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

* Make Timely generic over a container type

Add *Core variants of core Timely infrastructure that is generic in the
data passed along dataflow edges.

Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

* Introduce TimelyStack

This is mostly a copy of ColumnStack plus a few implementations. We'd like
to keep the columnar library mostly untouched and hence have our own
variant within Timely.

Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

timely/src/dataflow/channels/pushers/buffer.rs
@@ -1,44 +1,50 @@
 //! Buffering and session mechanisms to provide the appearance of record-at-a-time sending,
 //! with the performance of batched sends.
 
-use crate::dataflow::channels::{Bundle, Message};
+use crate::dataflow::channels::{Bundle, BundleCore, Message};
 use crate::progress::Timestamp;
 use crate::dataflow::operators::Capability;
 use crate::communication::Push;
+use crate::{Container, Data};
 
 /// Buffers data sent at the same time, for efficient communication.
 ///
 /// The `Buffer` type should be used by calling `session` with a time, which checks whether
 /// data must be flushed and creates a `Session` object which allows sending at the given time.
 #[derive(Debug)]
-pub struct Buffer<T, D, P: Push<Bundle<T, D>>> {
-    time: Option<T>,  // the currently open time, if it is open
-    buffer: Vec<D>,   // a buffer for records, to send at self.time
+pub struct BufferCore<T, D: Container, P: Push<BundleCore<T, D>>> {
+    /// the currently open time, if it is open
+    time: Option<T>,
+    /// a buffer for records, to send at self.time
+    buffer: D,
     pusher: P,
 }
 
-impl<T, D, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
+/// A buffer specialized to vector-based containers.
+pub type Buffer<T, D, P> = BufferCore<T, Vec<D>, P>;
+
+impl<T, C: Container, P: Push<BundleCore<T, C>>> BufferCore<T, C, P> where T: Eq+Clone {
 
     /// Creates a new `Buffer`.
-    pub fn new(pusher: P) -> Buffer<T, D, P> {
-        Buffer {
+    pub fn new(pusher: P) -> Self {
+        Self {
             time: None,
-            buffer: Vec::with_capacity(Message::<T, D>::default_length()),
+            buffer: Default::default(),
             pusher,
         }
     }
 
     /// Returns a `Session`, which accepts data to send at the associated time
-    pub fn session(&mut self, time: &T) -> Session<T, D, P> {
+    pub fn session(&mut self, time: &T) -> Session<T, C, P> {
         if let Some(true) = self.time.as_ref().map(|x| x != time) { self.flush(); }
         self.time = Some(time.clone());
         Session { buffer: self }
     }
     /// Allocates a new `AutoflushSession` which flushes itself on drop.
-    pub fn autoflush_session(&mut self, cap: Capability<T>) -> AutoflushSession<T, D, P> where T: Timestamp {
+    pub fn autoflush_session(&mut self, cap: Capability<T>) -> AutoflushSessionCore<T, C, P> where T: Timestamp {
         if let Some(true) = self.time.as_ref().map(|x| x != cap.time()) { self.flush(); }
         self.time = Some(cap.time().clone());
-        AutoflushSession {
+        AutoflushSessionCore {
             buffer: self,
             _capability: cap,
         }
@@ -63,8 +69,23 @@ impl<T, D, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
         }
     }
 
+    // Gives an entire container at a specific time.
+    fn give_container(&mut self, vector: &mut C) {
+        // flush to ensure fifo-ness
+        self.flush();
+
+        let time = self.time.as_ref().expect("Buffer::give_container(): time is None.").clone();
+        Message::push_at(vector, time, &mut self.pusher);
+    }
+}
+
+impl<T, D: Data, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
     // internal method for use by `Session`.
     fn give(&mut self, data: D) {
+        if self.buffer.capacity() < crate::container::buffer::default_capacity::<D>() {
+            let to_reserve = crate::container::buffer::default_capacity::<D>() - self.buffer.capacity();
+            self.buffer.reserve(to_reserve);
+        }
         self.buffer.push(data);
         // assert!(self.buffer.capacity() == Message::<O::Data>::default_length());
         if self.buffer.len() == self.buffer.capacity() {
@@ -75,9 +96,7 @@ impl<T, D, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
     // Gives an entire message at a specific time.
     fn give_vec(&mut self, vector: &mut Vec<D>) {
         // flush to ensure fifo-ness
-        if !self.buffer.is_empty() {
-            self.flush();
-        }
+        self.flush();
 
         let time = self.time.as_ref().expect("Buffer::give_vec(): time is None.").clone();
         Message::push_at(vector, time, &mut self.pusher);
@@ -90,11 +109,18 @@ impl<T, D, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
 /// The `Session` struct provides the user-facing interface to an operator output, namely
 /// the `Buffer` type. A `Session` wraps a session of output at a specified time, and
 /// avoids what would otherwise be a constant cost of checking timestamp equality.
-pub struct Session<'a, T, D, P: Push<Bundle<T, D>>+'a> where T: Eq+Clone+'a, D: 'a {
-    buffer: &'a mut Buffer<T, D, P>,
+pub struct Session<'a, T, C: Container, P: Push<BundleCore<T, C>>+'a> where T: Eq+Clone+'a, C: 'a {
+    buffer: &'a mut BufferCore<T, C, P>,
 }
 
-impl<'a, T, D, P: Push<Bundle<T, D>>+'a> Session<'a, T, D, P>  where T: Eq+Clone+'a, D: 'a {
+impl<'a, T, C: Container, P: Push<BundleCore<T, C>>+'a> Session<'a, T, C, P>  where T: Eq+Clone+'a, C: 'a {
+    /// Provide a container at the time specified by the [Session].
+    pub fn give_container(&mut self, container: &mut C) {
+        self.buffer.give_container(container)
+    }
+}
+
+impl<'a, T, D: Data, P: Push<BundleCore<T, Vec<D>>>+'a> Session<'a, T, Vec<D>, P>  where T: Eq+Clone+'a, D: 'a {
     /// Provides one record at the time specified by the `Session`.
     #[inline]
     pub fn give(&mut self, data: D) {
@@ -121,15 +147,18 @@ impl<'a, T, D, P: Push<Bundle<T, D>>+'a> Session<'a, T, D, P>  where T: Eq+Clone
 }
 
 /// A session which will flush itself when dropped.
-pub struct AutoflushSession<'a, T: Timestamp, D, P: Push<Bundle<T, D>>+'a> where
-    T: Eq+Clone+'a, D: 'a {
+pub struct AutoflushSessionCore<'a, T: Timestamp, C: Container, P: Push<BundleCore<T, C>>+'a> where
+    T: Eq+Clone+'a, C: 'a {
     /// A reference to the underlying buffer.
-    buffer: &'a mut Buffer<T, D, P>,
+    buffer: &'a mut BufferCore<T, C, P>,
     /// The capability being used to send the data.
     _capability: Capability<T>,
 }
 
-impl<'a, T: Timestamp, D, P: Push<Bundle<T, D>>+'a> AutoflushSession<'a, T, D, P> where T: Eq+Clone+'a, D: 'a {
+/// Auto-flush session specialized to vector-based containers.
+pub type AutoflushSession<'a, T, D, P> = AutoflushSessionCore<'a, T, Vec<D>, P>;
+
+impl<'a, T: Timestamp, D: Data, P: Push<BundleCore<T, Vec<D>>>+'a> AutoflushSessionCore<'a, T, Vec<D>, P> where T: Eq+Clone+'a, D: 'a {
     /// Transmits a single record.
     #[inline]
     pub fn give(&mut self, data: D) {
@@ -151,7 +180,7 @@ impl<'a, T: Timestamp, D, P: Push<Bundle<T, D>>+'a> AutoflushSession<'a, T, D, P
     }
 }
 
-impl<'a, T: Timestamp, D, P: Push<Bundle<T, D>>+'a> Drop for AutoflushSession<'a, T, D, P> where T: Eq+Clone+'a, D: 'a {
+impl<'a, T: Timestamp, C: Container, P: Push<BundleCore<T, C>>+'a> Drop for AutoflushSessionCore<'a, T, C, P> where T: Eq+Clone+'a, C: 'a {
     fn drop(&mut self) {
         self.buffer.cease();
     }

Cargo.toml
@@ -2,6 +2,7 @@
 members = [
     "bytes",
     "communication",
+    "container",
     "kafkaesque",
     "logging",
     "timely",

container/Cargo.toml
@@ -1,0 +1,11 @@
+[package]
+name = "timely_container"
+version = "0.12.0"
+edition = "2018"
+description = "Container abstractions for Timely"
+license = "MIT"
+
+[dependencies]
+columnation = { git = "https://github.com/antiguru/columnation", branch = "container" }
+#columnation = { path = "../../columnation/" }
+serde = { version = "1.0"}

container/src/columnation.rs
@@ -1,0 +1,294 @@
+//! A columnar container based on the columnation library.
+
+use std::iter::FromIterator;
+
+pub use columnation::*;
+
+/// An append-only vector that store records as columns.
+///
+/// This container maintains elements that might conventionally own
+/// memory allocations, but instead the pointers to those allocations
+/// reference larger regions of memory shared with multiple instances
+/// of the type. Elements can be retrieved as references, and care is
+/// taken when this type is dropped to ensure that the correct memory
+/// is returned (rather than the incorrect memory, from running the
+/// elements `Drop` implementations).
+pub struct TimelyStack<T: Columnation> {
+    local: Vec<T>,
+    inner: T::InnerRegion,
+}
+
+impl<T: Columnation> TimelyStack<T> {
+    /// Construct a [TimelyStack], reserving space for `capacity` elements
+    ///
+    /// Note that the associated region is not initialized to a specific capacity
+    /// because we can't generally know how much space would be required.
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            local: Vec::with_capacity(capacity),
+            inner: T::InnerRegion::default(),
+        }
+    }
+
+    /// Copies an element in to the region.
+    ///
+    /// The element can be read by indexing
+    pub fn copy(&mut self, item: &T) {
+        // TODO: Some types `T` should just be cloned.
+        // E.g. types that are `Copy` or vecs of ZSTs.
+        unsafe {
+            self.local.push(self.inner.copy(item));
+        }
+    }
+    /// Empties the collection.
+    pub fn clear(&mut self) {
+        unsafe {
+            // Unsafety justified in that setting the length to zero exposes
+            // no invalid data.
+            self.local.set_len(0);
+            self.inner.clear();
+        }
+    }
+    /// Retain elements that pass a predicate, from a specified offset.
+    ///
+    /// This method may or may not reclaim memory in the inner region.
+    pub fn retain_from<P: FnMut(&T) -> bool>(&mut self, index: usize, mut predicate: P) {
+        let mut write_position = index;
+        for position in index..self.local.len() {
+            if predicate(&self[position]) {
+                // TODO: compact the inner region and update pointers.
+                self.local.swap(position, write_position);
+                write_position += 1;
+            }
+        }
+        unsafe {
+            // Unsafety justified in that `write_position` is no greater than
+            // `self.local.len()` and so this exposes no invalid data.
+            self.local.set_len(write_position);
+        }
+    }
+
+    /// Unsafe access to `local` data. The slices stores data that is backed by a region
+    /// allocation. Therefore, it is undefined behavior to mutate elements of the `local` slice.
+    ///
+    /// Safety: Elements within `local` can be reordered, but not mutated, removed and/or dropped.
+    pub unsafe fn local(&mut self) -> &mut [T] {
+        &mut self.local[..]
+    }
+}
+
+impl<A: Columnation, B: Columnation> TimelyStack<(A, B)> {
+    /// Copies a destructured tuple `(A, B)` into this column stack.
+    ///
+    /// This serves situations where a tuple should be constructed from its constituents but not
+    /// not all elements are available as owned data.
+    ///
+    /// The element can be read by indexing
+    pub fn copy_destructured(&mut self, t1: &A, t2: &B) {
+        unsafe {
+            self.local.push(self.inner.copy_destructured(t1, t2));
+        }
+    }
+}
+
+impl<A: Columnation, B: Columnation, C: Columnation> TimelyStack<(A, B, C)> {
+    /// Copies a destructured tuple `(A, B, C)` into this column stack.
+    ///
+    /// This serves situations where a tuple should be constructed from its constituents but not
+    /// not all elements are available as owned data.
+    ///
+    /// The element can be read by indexing
+    pub fn copy_destructured(&mut self, r0: &A, r1: &B, r2: &C) {
+        unsafe {
+            self.local.push(self.inner.copy_destructured(r0, r1, r2));
+        }
+    }
+}
+
+impl<T: Columnation> std::ops::Deref for TimelyStack<T> {
+    type Target = [T];
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.local[..]
+    }
+}
+
+impl<T: Columnation> Drop for TimelyStack<T> {
+    fn drop(&mut self) {
+        self.clear();
+    }
+}
+
+impl<T: Columnation> Default for TimelyStack<T> {
+    fn default() -> Self {
+        Self {
+            local: Vec::new(),
+            inner: T::InnerRegion::default(),
+        }
+    }
+}
+
+impl<'a, A: 'a + Columnation> FromIterator<&'a A> for TimelyStack<A> {
+    fn from_iter<T: IntoIterator<Item = &'a A>>(iter: T) -> Self {
+        let mut iter = iter.into_iter();
+        let mut c = TimelyStack::<A>::with_capacity(iter.size_hint().0);
+        while let Some(element) = iter.next() {
+            c.copy(element);
+        }
+
+        c
+    }
+}
+
+impl<T: Columnation + PartialEq> PartialEq for TimelyStack<T> {
+    fn eq(&self, other: &Self) -> bool {
+        PartialEq::eq(&self[..], &other[..])
+    }
+}
+
+impl<T: Columnation + Eq> Eq for TimelyStack<T> {}
+
+impl<T: Columnation + std::fmt::Debug> std::fmt::Debug for TimelyStack<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        (&self[..]).fmt(f)
+    }
+}
+
+impl<T: Columnation> Clone for TimelyStack<T> {
+    fn clone(&self) -> Self {
+        let mut new: Self = Default::default();
+        for item in &self[..] {
+            new.copy(item);
+        }
+        new
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.clear();
+        for item in &source[..] {
+            self.copy(item);
+        }
+    }
+}
+
+mod serde {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    use crate::columnation::{Columnation, TimelyStack};
+
+    impl<T: Columnation + Serialize> Serialize for TimelyStack<T> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            use serde::ser::SerializeSeq;
+            let mut seq = serializer.serialize_seq(Some(self.local.len()))?;
+            for element in &self[..] {
+                seq.serialize_element(element)?;
+            }
+            seq.end()
+        }
+    }
+
+    impl<'a, T: Columnation + Deserialize<'a>> Deserialize<'a> for TimelyStack<T> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'a>,
+        {
+            use serde::de::{SeqAccess, Visitor};
+            use std::fmt;
+            use std::marker::PhantomData;
+            struct TimelyStackVisitor<T> {
+                marker: PhantomData<T>,
+            }
+
+            impl<'de, T: Columnation> Visitor<'de> for TimelyStackVisitor<T>
+            where
+                T: Deserialize<'de>,
+            {
+                type Value = TimelyStack<T>;
+
+                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                    formatter.write_str("a sequence")
+                }
+
+                fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+                where
+                    A: SeqAccess<'de>,
+                {
+                    let local = Vec::with_capacity(
+                        seq.size_hint()
+                            .unwrap_or(crate::buffer::default_capacity::<T>()),
+                    );
+                    let mut stack = TimelyStack {
+                        local,
+                        inner: T::InnerRegion::default(),
+                    };
+
+                    while let Some(value) = seq.next_element()? {
+                        stack.copy(&value);
+                    }
+
+                    Ok(stack)
+                }
+            }
+
+            let visitor = TimelyStackVisitor {
+                marker: PhantomData,
+            };
+            deserializer.deserialize_seq(visitor)
+        }
+    }
+}
+
+mod container {
+    use crate::{Container, PushPartitioned};
+
+    use crate::columnation::{Columnation, TimelyStack};
+
+    impl<T: Columnation + 'static> Container for TimelyStack<T> {
+        type Item = T;
+
+        fn len(&self) -> usize {
+            self.local.len()
+        }
+
+        fn is_empty(&self) -> bool {
+            self.local.is_empty()
+        }
+
+        fn capacity(&self) -> usize {
+           self.local.capacity()
+        }
+
+        fn clear(&mut self) {
+            TimelyStack::clear(self)
+        }
+    }
+
+    impl<T: Columnation + 'static> PushPartitioned for TimelyStack<T> {
+        fn push_partitioned<I, F>(&mut self, buffers: &mut [Self], mut index: I, mut flush: F)
+        where
+            I: FnMut(&Self::Item) -> usize,
+            F: FnMut(usize, &mut Self),
+        {
+            fn ensure_capacity<E: Columnation>(this: &mut TimelyStack<E>) {
+                let capacity = this.local.capacity();
+                let desired_capacity = crate::buffer::default_capacity::<E>();
+                if capacity < desired_capacity {
+                    this.local.reserve(desired_capacity - capacity);
+                }
+            }
+
+            for datum in &self[..] {
+                let index = index(&datum);
+                ensure_capacity(&mut buffers[index]);
+                buffers[index].copy(datum);
+                if buffers[index].len() == buffers[index].local.capacity() {
+                    flush(index, &mut buffers[index]);
+                }
+            }
+            self.clear();
+        }
+    }
+}

container/src/lib.rs
@@ -1,0 +1,165 @@
+//! Specifications for containers
+
+#![forbid(missing_docs)]
+
+pub mod columnation;
+
+/// A container transferring data through dataflow edges
+///
+/// A container stores a number of elements and thus is able to describe it length (`len()`) and
+/// whether it is empty (`is_empty()`). It supports removing all elements (`clear`).
+///
+/// A container must implement default. The default implementation is not required to allocate
+/// memory for variable-length components.
+///
+/// We require the container to be cloneable to enable efficient copies when providing references
+/// of containers to operators. Care must be taken that the type's `clone_from` implementation
+/// is efficient (which is not necessarily the case when deriving `Clone`.)
+/// TODO: Don't require `Container: Clone`
+pub trait Container: Default + Clone + 'static {
+    /// The type of elements this container holds.
+    type Item;
+
+    /// The number of elements in this container
+    ///
+    /// The length of a container must be consistent between sending and receiving it.
+    /// When exchanging a container and partitioning it into pieces, the sum of the length
+    /// of all pieces must be equal to the length of the original container.
+    fn len(&self) -> usize;
+
+    /// Determine if the container contains any elements, corresponding to `len() == 0`.
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// The capacity of the underlying container
+    fn capacity(&self) -> usize;
+
+    /// Remove all contents from `self` while retaining allocated memory.
+    /// After calling `clear`, `is_empty` must return `true` and `len` 0.
+    fn clear(&mut self);
+}
+
+impl<T: Clone + 'static> Container for Vec<T> {
+    type Item = T;
+
+    fn len(&self) -> usize {
+        Vec::len(self)
+    }
+
+    fn is_empty(&self) -> bool {
+        Vec::is_empty(self)
+    }
+
+    fn capacity(&self) -> usize {
+        Vec::capacity(self)
+    }
+
+    fn clear(&mut self) { Vec::clear(self) }
+}
+
+mod rc {
+    use std::rc::Rc;
+
+    use crate::Container;
+
+    impl<T: Container> Container for Rc<T> {
+        type Item = T::Item;
+
+        fn len(&self) -> usize {
+            std::ops::Deref::deref(self).len()
+        }
+
+        fn is_empty(&self) -> bool {
+            std::ops::Deref::deref(self).is_empty()
+        }
+
+        fn capacity(&self) -> usize {
+            std::ops::Deref::deref(self).capacity()
+        }
+
+        fn clear(&mut self) { }
+    }
+}
+
+mod arc {
+    use std::sync::Arc;
+
+    use crate::Container;
+
+    impl<T: Container> Container for Arc<T> {
+        type Item = T::Item;
+
+        fn len(&self) -> usize {
+            std::ops::Deref::deref(self).len()
+        }
+
+        fn is_empty(&self) -> bool {
+            std::ops::Deref::deref(self).is_empty()
+        }
+
+        fn capacity(&self) -> usize {
+            std::ops::Deref::deref(self).capacity()
+        }
+
+        fn clear(&mut self) { }
+    }
+}
+
+/// A container that can partition itself into pieces.
+pub trait PushPartitioned: Container {
+    /// Partition and push this container.
+    ///
+    /// Drain all elements from `self`, and use the function `index` to determine which `buffer` to
+    /// append an element to. Call `flush` with an index and a buffer to send the data downstream.
+    fn push_partitioned<I, F>(&mut self, buffers: &mut [Self], index: I, flush: F)
+    where
+        I: FnMut(&Self::Item) -> usize,
+        F: FnMut(usize, &mut Self);
+}
+
+impl<T: Clone + 'static> PushPartitioned for Vec<T> {
+    fn push_partitioned<I, F>(&mut self, buffers: &mut [Self], mut index: I, mut flush: F)
+    where
+        I: FnMut(&Self::Item) -> usize,
+        F: FnMut(usize, &mut Self),
+    {
+        fn ensure_capacity<E>(this: &mut Vec<E>) {
+            let capacity = this.capacity();
+            let desired_capacity = buffer::default_capacity::<E>();
+            if capacity < desired_capacity {
+                this.reserve(desired_capacity - capacity);
+            }
+        }
+
+        for datum in self.drain(..) {
+            let index = index(&datum);
+            ensure_capacity(&mut buffers[index]);
+            buffers[index].push(datum);
+            if buffers[index].len() == buffers[index].capacity() {
+                flush(index, &mut buffers[index]);
+            }
+        }
+    }
+}
+
+pub mod buffer {
+    //! Functionality related to calculating default buffer sizes
+
+    /// The upper limit for buffers to allocate, size in bytes. [default_capacity] converts
+    /// this to size in elements.
+    pub const BUFFER_SIZE_BYTES: usize = 1 << 13;
+
+    /// The maximum buffer capacity in elements. Returns a number between [:BUFFER_SIZE_BYTES]
+    /// and 1, inclusively.
+    pub const fn default_capacity<T>() -> usize {
+        let size = ::std::mem::size_of::<T>();
+        if size == 0 {
+            BUFFER_SIZE_BYTES
+        } else if size <= BUFFER_SIZE_BYTES {
+            BUFFER_SIZE_BYTES / size
+        } else {
+            1
+        }
+    }
+}

kafkaesque/src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicIsize, Ordering};
 
 use abomonation::Abomonation;
-use timely::dataflow::operators::capture::event::{Event, EventPusher, EventIterator};
+use timely::dataflow::operators::capture::event::{EventCore, EventPusherCore, EventIteratorCore};
 
 use rdkafka::Message;
 use rdkafka::client::ClientContext;
@@ -37,7 +37,7 @@ impl OutstandingCounterContext {
 }
 
 /// A wrapper for `W: Write` implementing `EventPusher<T, D>`.
-pub struct EventProducer<T, D> {
+pub struct EventProducerCore<T, D> {
     topic: String,
     buffer: Vec<u8>,
     producer: BaseProducer<OutstandingCounterContext>,
@@ -45,14 +45,17 @@ pub struct EventProducer<T, D> {
     phant: ::std::marker::PhantomData<(T,D)>,
 }
 
-impl<T, D> EventProducer<T, D> {
+/// [EventProducerCore] specialized to vector-based containers.
+pub type EventProducer<T, D> = EventProducerCore<T, Vec<D>>;
+
+impl<T, D> EventProducerCore<T, D> {
     /// Allocates a new `EventWriter` wrapping a supplied writer.
     pub fn new(config: ClientConfig, topic: String) -> Self {
         let counter = Arc::new(AtomicIsize::new(0));
         let context = OutstandingCounterContext::new(&counter);
         let producer = BaseProducer::<OutstandingCounterContext>::from_config_and_context(&config, context).expect("Couldn't create producer");
         println!("allocating producer for topic {:?}", topic);
-        EventProducer {
+        Self {
             topic: topic,
             buffer: vec![],
             producer: producer,
@@ -62,8 +65,8 @@ impl<T, D> EventProducer<T, D> {
     }
 }
 
-impl<T: Abomonation, D: Abomonation> EventPusher<T, D> for EventProducer<T, D> {
-    fn push(&mut self, event: Event<T, D>) {
+impl<T: Abomonation, D: Abomonation> EventPusherCore<T, D> for EventProducerCore<T, D> {
+    fn push(&mut self, event: EventCore<T, D>) {
         unsafe { ::abomonation::encode(&event, &mut self.buffer).expect("Encode failure"); }
         // println!("sending {:?} bytes", self.buffer.len());
         self.producer.send::<(),[u8]>(BaseRecord::to(self.topic.as_str()).payload(&self.buffer[..])).unwrap();
@@ -73,7 +76,7 @@ impl<T: Abomonation, D: Abomonation> EventPusher<T, D> for EventProducer<T, D> {
     }
 }
 
-impl<T, D> Drop for EventProducer<T, D> {
+impl<T, D> Drop for EventProducerCore<T, D> {
     fn drop(&mut self) {
         while self.counter.load(Ordering::SeqCst) > 0 {
             self.producer.poll(std::time::Duration::from_millis(10));
@@ -82,19 +85,22 @@ impl<T, D> Drop for EventProducer<T, D> {
 }
 
 /// A Wrapper for `R: Read` implementing `EventIterator<T, D>`.
-pub struct EventConsumer<T, D> {
+pub struct EventConsumerCore<T, D> {
     consumer: BaseConsumer<DefaultConsumerContext>,
     buffer: Vec<u8>,
     phant: ::std::marker::PhantomData<(T,D)>,
 }
 
-impl<T, D> EventConsumer<T, D> {
+/// [EventConsumerCore] specialized to vector-based containers.
+pub type EventConsumer<T, D> = EventConsumerCore<T, Vec<D>>;
+
+impl<T, D> EventConsumerCore<T, D> {
     /// Allocates a new `EventReader` wrapping a supplied reader.
     pub fn new(config: ClientConfig, topic: String) -> Self {
         println!("allocating consumer for topic {:?}", topic);
         let consumer : BaseConsumer<DefaultConsumerContext> = config.create().expect("Couldn't create consumer");
         consumer.subscribe(&[&topic]).expect("Failed to subscribe to topic");
-        EventConsumer {
+        Self {
             consumer: consumer,
             buffer: Vec::new(),
             phant: ::std::marker::PhantomData,
@@ -102,14 +108,14 @@ impl<T, D> EventConsumer<T, D> {
     }
 }
 
-impl<T: Abomonation, D: Abomonation> EventIterator<T, D> for EventConsumer<T, D> {
-    fn next(&mut self) -> Option<&Event<T, D>> {
+impl<T: Abomonation, D: Abomonation> EventIteratorCore<T, D> for EventConsumerCore<T, D> {
+    fn next(&mut self) -> Option<&EventCore<T, D>> {
         if let Some(result) = self.consumer.poll(std::time::Duration::from_millis(0)) {
             match result {
                 Ok(message) =>  {
                     self.buffer.clear();
                     self.buffer.extend_from_slice(message.payload().unwrap());
-                    Some(unsafe { ::abomonation::decode::<Event<T,D>>(&mut self.buffer[..]).unwrap().0 })
+                    Some(unsafe { ::abomonation::decode::<EventCore<T,D>>(&mut self.buffer[..]).unwrap().0 })
                 },
                 Err(err) => {
                     println!("KafkaConsumer error: {:?}", err);

timely/Cargo.toml
@@ -29,6 +29,7 @@ abomonation_derive = "0.5"
 timely_bytes = { path = "../bytes", version = "0.12" }
 timely_logging = { path = "../logging", version = "0.12" }
 timely_communication = { path = "../communication", version = "0.12", default-features = false }
+timely_container = { path = "../container", version = "0.12" }
 crossbeam-channel = "0.5.0"
 futures-util = "0.3"
 

commit 9f2b81d02d891ab3d7739ccd733c2963163bdbc8

Inline give for Buffer
This fixes a regression introduced by #426 where BufferCore::give now
lacked the inline annotation. In pingpong, this changes the runtime as
follows:

Without inline:
target/release/examples/pingpong 1 500000000 -w1  2.45s user 0.00s system 99% cpu 2.446 total

With inline:
target/release/examples/pingpong 1 500000000 -w1  1.75s user 0.00s system 99% cpu 1.755 total

Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

timely/src/dataflow/channels/pushers/buffer.rs
@@ -81,6 +81,7 @@ impl<T, C: Container, P: Push<BundleCore<T, C>>> BufferCore<T, C, P> where T: Eq
 
 impl<T, D: Data, P: Push<Bundle<T, D>>> Buffer<T, D, P> where T: Eq+Clone {
     // internal method for use by `Session`.
+    #[inline]
     fn give(&mut self, data: D) {
         if self.buffer.capacity() < crate::container::buffer::default_capacity::<D>() {
             let to_reserve = crate::container::buffer::default_capacity::<D>() - self.buffer.capacity();