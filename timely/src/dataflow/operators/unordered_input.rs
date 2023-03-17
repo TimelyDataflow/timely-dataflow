ecb666f0862d4e6d58d79bebc3c518e3d1829188
Moritz Hoffmann <antiguru@gmail.com>
2023-03-16 21:25:58 -0400

    Promote UnorderedInputCore to main variant
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- timely/src/dataflow/operators/unordered_input.rs
+++ timely/src/dataflow/operators/unordered_input.rs
@@ -21,11 +21,11 @@ use crate::dataflow::{Scope, Stream};
 
 /// Create a new `Stream` and `Handle` through which to supply input.
 pub trait UnorderedInput<G: Scope> {
-    /// Create a new capability-based `Stream` and `Handle` through which to supply input. This
+    /// Create a new capability-based [Stream] and [UnorderedHandle] through which to supply input. This
     /// input supports multiple open epochs (timestamps) at the same time.
     ///
-    /// The `new_unordered_input` method returns `((Handle, Capability), Stream)` where the `Stream` can be used
-    /// immediately for timely dataflow construction, `Handle` and `Capability` are later used to introduce
+    /// The `new_unordered_input_core` method returns `((HandleCore, Capability), Stream)` where the `Stream` can be used
+    /// immediately for timely dataflow construction, `HandleCore` and `Capability` are later used to introduce
     /// data into the timely dataflow computation.
     ///
     /// The `Capability` returned is for the default value of the timestamp type in use. The
@@ -72,75 +72,12 @@ pub trait UnorderedInput<G: Scope> {
     ///     assert_eq!(extract[i], (i, vec![i]));
     /// }
     /// ```
-    fn new_unordered_input<D:Data>(&mut self) -> ((UnorderedHandle<G::Timestamp, Vec<D>>, ActivateCapability<G::Timestamp>), Stream<G, Vec<D>>);
+    fn new_unordered_input<D: Container>(&mut self) -> ((UnorderedHandle<G::Timestamp, D>, ActivateCapability<G::Timestamp>), Stream<G, D>);
 }
 
 
 impl<G: Scope> UnorderedInput<G> for G {
-    fn new_unordered_input<D:Data>(&mut self) -> ((UnorderedHandle<G::Timestamp, Vec<D>>, ActivateCapability<G::Timestamp>), Stream<G, Vec<D>>) {
-        self.new_unordered_input_core()
-    }
-}
-
-/// Create a new `Stream` and `Handle` through which to supply input.
-pub trait UnorderedInputCore<G: Scope> {
-    /// Create a new capability-based [Stream] and [UnorderedHandle] through which to supply input. This
-    /// input supports multiple open epochs (timestamps) at the same time.
-    ///
-    /// The `new_unordered_input_core` method returns `((HandleCore, Capability), Stream)` where the `Stream` can be used
-    /// immediately for timely dataflow construction, `HandleCore` and `Capability` are later used to introduce
-    /// data into the timely dataflow computation.
-    ///
-    /// The `Capability` returned is for the default value of the timestamp type in use. The
-    /// capability can be dropped to inform the system that the input has advanced beyond the
-    /// capability's timestamp. To retain the ability to send, a new capability at a later timestamp
-    /// should be obtained first, via the `delayed` function for `Capability`.
-    ///
-    /// To communicate the end-of-input drop all available capabilities.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use std::sync::{Arc, Mutex};
-    ///
-    /// use timely::*;
-    /// use timely::dataflow::operators::*;
-    /// use timely::dataflow::operators::capture::Extract;    ///     ///
-    /// // get send and recv endpoints, wrap send to share
-    /// let (send, recv) = ::std::sync::mpsc::channel();
-    /// let send = Arc::new(Mutex::new(send));
-    ///
-    /// timely::execute(Config::thread(), move |worker| {
-    ///
-    ///     // this is only to validate the output.
-    ///     let send = send.lock().unwrap().clone();
-    ///
-    ///     // create and capture the unordered input.
-    ///     let (mut input, mut cap) = worker.dataflow::<usize,_,_>(|scope| {
-    ///         let (input, stream) = scope.new_unordered_input_core();
-    ///         stream.capture_into(send);
-    ///         input
-    ///     });
-    ///
-    ///     // feed values 0..10 at times 0..10.
-    ///     for round in 0..10 {
-    ///         input.session(cap.clone()).give(round);
-    ///         cap = cap.delayed(&(round + 1));
-    ///         worker.step();
-    ///     }
-    /// }).unwrap();
-    ///
-    /// let extract = recv.extract();
-    /// for i in 0..10 {
-    ///     assert_eq!(extract[i], (i, vec![i]));
-    /// }
-    /// ```
-    fn new_unordered_input_core<D: Container>(&mut self) -> ((UnorderedHandle<G::Timestamp, D>, ActivateCapability<G::Timestamp>), Stream<G, D>);
-}
-
-
-impl<G: Scope> UnorderedInputCore<G> for G {
-    fn new_unordered_input_core<D: Container>(&mut self) -> ((UnorderedHandle<G::Timestamp, D>, ActivateCapability<G::Timestamp>), Stream<G, D>) {
+    fn new_unordered_input<D: Container>(&mut self) -> ((UnorderedHandle<G::Timestamp, D>, ActivateCapability<G::Timestamp>), Stream<G, D>) {
 
         let (output, registrar) = Tee::<G::Timestamp, D>::new();
         let internal = Rc::new(RefCell::new(ChangeBatch::new()));
