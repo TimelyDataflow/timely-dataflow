e9ad539adbd870d0ec345cdc7d760866b0ebb1f8
Frank McSherry <fmcsherry@me.com>
2024-03-21 15:30:40 -0400

    Operator movement (into `core`) (#553)
    * Update Feedback
    
    * Update Concat
    
    * Update Rc
    
    * Update Probe

--- timely/src/dataflow/operators/core/mod.rs
+++ timely/src/dataflow/operators/core/mod.rs
@@ -1,10 +1,17 @@
 //! Extension traits for `Stream` implementing various operators that
 //! are independent of specific container types.
 
+pub mod concat;
 pub mod exchange;
+pub mod feedback;
 pub mod inspect;
+pub mod probe;
+pub mod rc;
 pub mod reclock;
 
+pub use concat::{Concat, Concatenate};
 pub use exchange::Exchange;
+pub use feedback::{Feedback, LoopVariable, ConnectLoop};
 pub use inspect::{Inspect, InspectCore};
+pub use probe::Probe;
 pub use reclock::Reclock;
