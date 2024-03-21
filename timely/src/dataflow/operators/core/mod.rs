d28b14d10c1964f01db9490fb41f05e589039303
Frank McSherry <fmcsherry@me.com>
2024-03-21 14:53:09 -0400

    Update Probe

--- timely/src/dataflow/operators/core/mod.rs
+++ timely/src/dataflow/operators/core/mod.rs
@@ -5,6 +5,7 @@ pub mod concat;
 pub mod exchange;
 pub mod feedback;
 pub mod inspect;
+pub mod probe;
 pub mod rc;
 pub mod reclock;
 
@@ -12,4 +13,5 @@ pub use concat::{Concat, Concatenate};
 pub use exchange::Exchange;
 pub use feedback::{Feedback, LoopVariable, ConnectLoop};
 pub use inspect::{Inspect, InspectCore};
+pub use probe::Probe;
 pub use reclock::Reclock;
