3f9d3874c6c5d75e2a9e67af9e78d6c40234be6a
Moritz Hoffmann <antiguru@gmail.com>
2024-07-01 22:15:55 -0400

    Flat container storage abstraction
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- container/src/flatcontainer.rs
+++ container/src/flatcontainer.rs
@@ -1,9 +1,14 @@
 //! Present a [`FlatStack`] as a timely container.
 
 pub use flatcontainer::*;
+use flatcontainer::impls::index::IndexContainer;
 use crate::{buffer, Container, SizableContainer, PushInto};
 
-impl<R: Region + Clone + 'static> Container for FlatStack<R> {
+impl<R, S> Container for FlatStack<R, S>
+where
+    R: Region + Clone + 'static,
+    S: IndexContainer<<R as Region>::Index> + Clone + 'static,
+{
     type ItemRef<'a> = R::ReadItem<'a>  where Self: 'a;
     type Item<'a> = R::ReadItem<'a> where Self: 'a;
 
@@ -28,6 +33,7 @@ impl<R: Region + Clone + 'static> Container for FlatStack<R> {
     }
 }
 
+// Only implemented for `FlatStack` with `Vec` offsets.
 impl<R: Region + Clone + 'static> SizableContainer for FlatStack<R> {
     fn capacity(&self) -> usize {
         self.capacity()
@@ -42,7 +48,11 @@ impl<R: Region + Clone + 'static> SizableContainer for FlatStack<R> {
     }
 }
 
-impl<R: Region + Push<T>, T> PushInto<T> for FlatStack<R> {
+impl<R, S, T> PushInto<T> for FlatStack<R, S>
+where
+    R: Region + Push<T>,
+    S: IndexContainer<R::Index> + Clone + 'static,
+{
     #[inline]
     fn push_into(&mut self, item: T) {
         self.copy(item);
