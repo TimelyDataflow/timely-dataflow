63103bd5f7584fd77b3256580df229189cbcd314
Moritz Hoffmann <antiguru@gmail.com>
2024-07-09 10:36:29 -0400

    Introduce CapacityContainer
    Signed-off-by: Moritz Hoffmann <antiguru@gmail.com>

--- container/src/flatcontainer.rs
+++ container/src/flatcontainer.rs
@@ -2,7 +2,7 @@
 
 pub use flatcontainer::*;
 use flatcontainer::impls::index::IndexContainer;
-use crate::{buffer, Container, SizableContainer, PushInto};
+use crate::{buffer, Container, SizableContainer, PushInto, CapacityContainer};
 
 impl<R, S> Container for FlatStack<R, S>
 where
@@ -45,6 +45,25 @@ impl<R: Region + Clone + 'static> SizableContainer for FlatStack<R> {
 
     fn reserve(&mut self, additional: usize) {
         self.reserve(additional);
+    }
+}
+
+impl<R, S> CapacityContainer for FlatStack<R, S>
+{
+    fn preferred_capacity() -> usize {
+        // We don't have a good way to present any pre-defined capacity here, since it's a
+        // concept foreign to flat containers. Each region might have a capacity, but overall
+        // the concept of capacity does not exist. For this reason, we just hardcode a number,
+        // which seems to work reasonably well.
+        //
+        // We should revisit this if/once we have an abstraction that can express a capacity
+        // for `FlatStack`, but we aren't there yet.
+        1024
+    }
+
+    fn ensure_preferred_capacity(&mut self) {
+        // Nop, same reasoning as for `preferred_capacity`. We don't know how to ensure capacity
+        // for a certain number of elements.
     }
 }
 
