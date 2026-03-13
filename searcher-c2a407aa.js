8de2b0dfd8bf048778a3cba6326fbf10e570195f
frankmcsherry <fmcsherry@me.com>
2026-03-13 09:55:18 +0000

    Deploying to gh-pages from @ TimelyDataflow/timely-dataflow@9319cabc60dbec10bedc935e43cacca8c6b15a8b 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8b624a9b.js',
+                path_to_root + 'searchindex-192e2cad.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
